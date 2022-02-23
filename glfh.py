ea7e6ffe457349d39bbdadfdaf74a2d7284cf3b0
Rasmus Hansen <rasmus14326@gmail.com>
2022-02-23 14:23:54 +0100

    unbroke normal mode

--- glfh.py
+++ glfh.py
@@ -57,6 +57,7 @@ def run(test: bool):
 
 if __name__ == '__main__':
     test = False 
-    if sys.argv[1] == '--test' or sys.argv[1] == '-t':
-        test = True
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--test' or sys.argv[1] == '-t':
+            test = True
     run(test)
